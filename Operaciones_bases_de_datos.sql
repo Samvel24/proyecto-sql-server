a91995fb246892ec18e05d5e701cc713f80c62ed
SamuelR <s_ramirezg_0624@outlook.com>
2025-01-29 23:57:17 -0600

    Operaciones especificas para bases de datos

---
+++ Operaciones_bases_de_datos.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE NombreBaseDeDatos; -- Instrucción para crear un base de datos
+DROP DATABASE NombreBaseDeDatos; -- Instrucción para borrar una base de datos
+SELECT * FROM sys.databases; -- Instrucción para mostrar las bases de datos de sistema
+ALTER DATABASE NombreBaseDeDatos MODIFY NAME = NuevoNombreBaseDeDatos; -- Instrucción para cambiar el nombre de una base de datos
