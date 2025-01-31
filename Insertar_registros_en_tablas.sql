1fca938a674813a0669ae1588bee236f8eea9fbc
SamuelR <s_ramirezg_0624@outlook.com>
2025-01-30 19:00:07 -0600

    Se ha colocado una instrucción para mostrar detalles sobre los campos de una tabla

--- Insertar_registros_en_tablas.sql
+++ Insertar_registros_en_tablas.sql
@@ -1,3 +1,11 @@
+exec sp_help Empleados;
+/*
+exec ejecuta una cadena de comandos o una cadena de caracteres dentro de un lote Transact-SQL,
+sp_help informa sobre un objeto de base de datos, un tipo de datos definido por el usuario 
+o un tipo de datos.
+Por tanto, esta instrucción ejecuta el comando sp_help que nos informa sobre un objeto de 
+base de datos llamado Empleados, en este caso la tabla Empleados.
+*/
 insert into Empleados values( -- Insertar datos en una fila de la tabla Empleados, para el idEmpleado=1
 1, 'Mario', 'Ramírez', 27, 1234567890, 'Calle 23, No. 9', 
 '1980-05-25', 8500, 'SI');
