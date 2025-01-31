644c4836a9ce0df4320c5551eef8fb6f67abc201
SamuelR <s_ramirezg_0624@outlook.com>
2025-01-30 18:46:59 -0600

    Tipos de datos en sql server

---
+++ Tipos_de_datos.sql
@@ -0,0 +1,18 @@
+/*
+Tipos de datos
+En el motor de base de datos de SQL Server, cada columna, variable local, expresión y parámetro 
+tiene un tipo de datos relacionado. Un tipo de datos es un atributo que especifica el tipo 
+de datos que puede contener el objeto: datos enteros, datos de caracteres, datos monetarios, 
+datos de fecha y hora, cadenas binarias, etc.
+
+Los tipos de datos en SQL Server se organizan en las siguientes categorías:
+    • Cifras exactas
+    • Cifras numéricas aproximadas
+    • Fecha y hora
+    • Cadenas de caracteres
+    • Cadenas de caracteres Unicode
+    • Cadenas binarias
+    • Otros tipos de datos
+
+Fuente: https://learn.microsoft.com/en-us/sql/t-sql/data-types/data-types-transact-sql?view=sql-server-ver16    
+*/
