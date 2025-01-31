a58de2b0ac96c8de660fd1a53785b0d1fc10058d
SamuelR <s_ramirezg_0624@outlook.com>
2025-01-30 18:47:36 -0600

    Instrucciones para insertar registros en tablas

---
+++ Insertar_registros_en_tablas.sql
@@ -0,0 +1,9 @@
+insert into Empleados values( -- Insertar datos en una fila de la tabla Empleados, para el idEmpleado=1
+1, 'Mario', 'Ramírez', 27, 1234567890, 'Calle 23, No. 9', 
+'1980-05-25', 8500, 'SI');
+
+insert into Empleados values( -- Insertar datos en una fila de la tabla Empleados, para el idEmpleado=2
+2, 'Rodrigo', 'Morales', 35, 1234337890, 'Calle 30, No. 1', 
+'1990-05-31', 9500, 'NO');
+
+select * from Empleados; -- Mostrar todos los registros de la tabla Empleados
