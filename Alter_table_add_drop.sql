1a799f0feb5e43bf658fe57c2c1e8ae3132d26b3
Samuel Ramírez <s_ramirezg_0624@outlook.com>
2025-02-06 21:22:04 -0600

    Instrucción alter para agregar y quitar columnas en una tabla

---
+++ Alter_table_add_drop.sql
@@ -0,0 +1,5 @@
+alter table Empleados add genero char(1); -- Agrega la columna genero de tipo char a la tabla Empleados
+
+alter table Empleados add fecha_contratacion date; -- Agrega la columna fecha_contratacion de tipo date a la tabla Empleados
+
+alter table NombreTabla drop column NombreCampo; -- Con esta instrucción borramos el campo proporcionado en una tabla determinada
