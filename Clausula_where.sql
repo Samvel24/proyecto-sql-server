7028ffa998c258b02c6f34981a8123c6e4a28e7f
Samuel Ramírez <s_ramirezg_0624@outlook.com>
2025-02-06 20:05:34 -0600

    Ejemplos de uso de la clasula where

---
+++ Clausula_where.sql
@@ -0,0 +1,5 @@
+select * from Empleados where nombre = 'Jorge'; -- Devuelve todos los registros de la tabla Empleados cuyo nombre sea Jorge
+
+select * from Empleados where edad = 29; -- Devuelve todos los registros de la tabla Empleados cuyo edad sea 29
+
+select nombre, apellido from Empleados where edad = 29; -- Regresa el nombre y apellido de aquellos Empleados cuya edad sea 29
