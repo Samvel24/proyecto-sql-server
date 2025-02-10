bfb74724f7001b017f00a91246850fcbd0782f25
Samuel Ramírez <s_ramirezg_0624@outlook.com>
2025-02-09 18:28:18 -0600

    Uso de operadores relacionales

---
+++ Operadores_relacionales.sql
@@ -0,0 +1,11 @@
+select * from Empleados where idEmpleado = 3; -- Devuelve el empleado cuyo id es igual a 3
+
+select * from Empleados where edad != 25; -- Devuelve los empleados cuya edad es diferente de 25
+
+select * from Empleados where <> 25; -- Devuelve los empleados cuya edad es diferente de 25
+
+select * from Empleados where salario > 2000; -- Devuelve los empleados con salario mayor a 2000
+
+select * from Empleados where salario < 3000; -- Devuelve los empleados con salario menor a 3000
+
+select * from Empleados where salario <= 2500; -- Devuelve los empleados con salario menor o igual a 2500 
